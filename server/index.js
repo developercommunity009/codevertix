823081d23d1c339c117ee2130efd789f86765f0f
developercommunity009 <108273895+developercommunity009@users.noreply.github.com>
2024-04-27 08:22:39 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -40,27 +40,26 @@ res.send("Hello CodeVertix")
 
 // Route to send email
 // Route to handle form submission and send email
-app.get('/send-email', (req, res) => {
-  res.send("Send mail")
-    // const { name, email, message } = req.body;
-    // // Email options
-    // const mailOptions = {
-    //     from: 'zahranisoft8@gmail.com',
-    //     to: 'Hr@codevertix.com',
-    //     subject: `CODEVERIX User ${name}  `,
-    //     text: `User Name: ${name}\nUser Email: ${email}\nUser Message: ${message}`
-    // };
+app.post('/send-email', (req, res) => {
+    const { name, email, message } = req.body;
+    // Email options
+    const mailOptions = {
+        from: 'zahranisoft8@gmail.com',
+        to: 'Hr@codevertix.com',
+        subject: `CODEVERIX User ${name}  `,
+        text: `User Name: ${name}\nUser Email: ${email}\nUser Message: ${message}`
+    };
 
-    // // Send email
-    // transporter.sendMail(mailOptions, (error, info) => {
-    //     if (error) {
-    //         console.error('Error occurred:', error);
-    //         res.status(500).send('An error occurred while sending the email.');
-    //     } else {
-    //         console.log('Email sent:', info.response);
-    //         res.send('Email sent successfully!');
-    //     }
-    // });
+    // Send email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error occurred:', error);
+            res.status(500).send('An error occurred while sending the email.');
+        } else {
+            console.log('Email sent:', info.response);
+            res.send('Email sent successfully!');
+        }
+    });
 });
 
 
