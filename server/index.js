7f04cc9f124dff5645e2722103c038dc59feaf3a
developercommunity009 <108273895+developercommunity009@users.noreply.github.com>
2024-04-29 13:26:45 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: `http://www.codevertix.com/`,
+  origin: `https://www.codevertix.com/`,
 methods: ['GET', 'POST'],
   credentials: true,
 }));
